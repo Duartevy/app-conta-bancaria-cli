1ad4a68ec794305a8995cc3bdbf58110a2f79908
Duartevy <milenegba@gmail.com>
2025-06-01 20:27:07 -0300

    aplicação finalizada ContaBanco com entrada via terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        // Criação do Scanner para entrada de dados via terminal
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitação de dados para o usuário
+        System.out.println("Por favor, digite o número da Conta");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer do enter
+
+
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número do saldo");
+        double saldo = scanner.nextDouble();
+
+        Locale brasil = Locale.of("pt", "BR");
+        NumberFormat formatoMoeda  = NumberFormat.getCurrencyInstance(brasil);
+        String saldoFormatado = formatoMoeda.format(saldo); 
+
+        // TODO: Exibir a mensagem conta criada 
+        System.out.println(" Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " + 
+                            "sua agência é " + agencia + ", conta " + numero + 
+                            " e seu saldo " + saldoFormatado + 
+                            " já está disponível para saque.");
+
+        scanner.close();
     }
 }
